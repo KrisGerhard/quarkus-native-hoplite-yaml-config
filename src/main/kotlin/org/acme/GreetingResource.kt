18277e4b8ed43bca27d882cf73aeddf6f8d6f845
Kris-Gerhard Aabrams <kris@dolmit.com>
2021-03-04 10:13:15 +0200

    Add logging to demonstrate an issue

--- src/main/kotlin/org/acme/GreetingResource.kt
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -1,6 +1,7 @@
 package org.acme
 
 import com.sksamuel.hoplite.ConfigLoader
+import org.slf4j.LoggerFactory
 import java.nio.file.Path.of
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -10,9 +11,17 @@ import javax.ws.rs.core.MediaType
 @Path("/hello-resteasy")
 class GreetingResource {
 
+    private final val log = LoggerFactory.getLogger(this::class.java.simpleName)
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello(): String {
+        Config::class.constructors.forEach { constructor ->
+            constructor.parameters.map { parameter ->
+                log.info("constructor parameter type: " + (parameter.type).toString())
+                log.info("constructor parameter type classifier: " + (parameter.type.classifier).toString())
+            }
+        }
         val config = ConfigLoader().loadConfigOrThrow<Config>(of("application.yaml"))
         return config.greetingText
     }
